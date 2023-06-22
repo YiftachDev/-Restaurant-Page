2a062178c026d56dbb4172292421d2e8a6556640
YiftachDev <yiftach007@gmail.com>
2023-06-22 13:22:18 +0300

    Orginized the code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import home from "./Home";
 
-function init() {
-  const content = document.querySelector("#content");
-  content.innerHTML = `<div class="navbar">
+const flow = (function () {
+  const defaultContent = `<div class="navbar">
     <a href="#" class="hover-underline-animation">Home</a>
     <a href="#" class="hover-underline-animation">Menu</a>
     <a href="#" class="hover-underline-animation">Contact Us</a>
     </div>`;
-  home(content);
-}
+  const content = document.querySelector("#content");
 
-window.onload = init;
+  function init() {
+    content.innerHTML = defaultContent;
+    home(content);
+  }
+
+  function resetContent() {
+    content.innerHTML = defaultContent;
+  }
+
+  window.onload = init;
+})();
