7f74c2ca70ab29e9430b66e33f1247d3d84e5f16
YiftachDev <yiftach007@gmail.com>
2023-06-22 13:15:37 +0300

    Created homefile

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"hello!\");\r\n\n\n//# sourceURL=webpack://restorant/./src/index.js?");
+eval("function init() {\r\n  const content = document.querySelector(\"#content\");\r\n  content.innerHTML = `<div class=\"navbar\">\r\n    <a href=\"#\" class=\"hover-underline-animation\">Home</a>\r\n    <a href=\"#\" class=\"hover-underline-animation\">Menu</a>\r\n    <a href=\"#\" class=\"hover-underline-animation\">Contact Us</a>\r\n    </div>\r\n    <div class=\"container\">\r\n    <h1>Pizza de la Kizza</h1>\r\n    <div class=\"section\">\r\n        <h2>About Us</h2>\r\n        <p>\r\n        Pizza de la Kizza is a vibrant pizzeria in [City/Town] offering\r\n        handcrafted pizzas with quality ingredients, diverse menu options,\r\n        and exceptional service. Whether dining in or ordering online, they\r\n        provide a delightful pizza experience that satisfies all tastes.\r\n        </p>\r\n    </div>\r\n    <div class=\"section\">\r\n        <h2>Hours</h2>\r\n        <ul>\r\n        <li>Monday: 11:00 AM - 9:00 PM</li>\r\n        <li>Tuesday: 11:00 AM - 9:00 PM</li>\r\n        <li>Wednesday: 11:00 AM - 9:00 PM</li>\r\n        <li>Thursday: 11:00 AM - 9:00 PM</li>\r\n        <li>Friday: 11:00 AM - 10:00 PM</li>\r\n        </ul>\r\n    </div>\r\n    <div class=\"section\">\r\n        <h2>Location</h2>\r\n        <p>located in the charming town of Portofino, Italy.</p>\r\n    </div>\r\n    </div>`;\r\n}\r\n\r\nwindow.onload = init;\r\n\n\n//# sourceURL=webpack://restorant/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello!");
+import home from "./Home";
+
+function init() {
+  const content = document.querySelector("#content");
+  content.innerHTML = `<div class="navbar">
+    <a href="#" class="hover-underline-animation">Home</a>
+    <a href="#" class="hover-underline-animation">Menu</a>
+    <a href="#" class="hover-underline-animation">Contact Us</a>
+    </div>`;
+  home(content);
+}
+
+window.onload = init;
